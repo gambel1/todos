b13eb8777725845d898bf9ae71279d97074cb588
Алексей <electrifyx@gmail.com>
2023-10-13 15:29:39 +0300

    testing project

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App/App';
 
-ReactDOM.createRoot(document.getElementById('root')as HTMLElement).render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// const root = ReactDOM.createRoot(
-//   document.getElementById('root') as HTMLElement
-// );
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 
